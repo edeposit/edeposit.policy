5257630971614eaa1dca853d184293ca16f56fd9
Jan Stavěl <stavel.jan@gmail.com>
2013-08-26 09:44:49 +0200

    small fixes of description.

--- edeposit/policy/__init__.py
+++ edeposit/policy/__init__.py
@@ -1,4 +1,6 @@
-  # -*- extra stuff goes here -*- 
+import zope.i18nmessageid
+
+MessageFactory = zope.i18nmessageid.MessageFactory('edeposit.policy')
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
