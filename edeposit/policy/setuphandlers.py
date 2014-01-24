d5dc1fb2848547793febd14280d78ae52ef16eb6
Jan Stavěl <stavel.jan@gmail.com>
2014-01-24 21:44:44 +0100

    added setuphandlers.

--- edeposit/policy/setuphandlers.py
+++ edeposit/policy/setuphandlers.py
@@ -1,22 +1,43 @@
+# -*- coding: utf-8 -*-
 from edeposit.policy import MessageFactory as _
 from Products.CMFCore.utils import getToolByName
 
-groups = (
+groups = (   
     { 'group_id': 'Acquisitors',
       'title': 'E-Deposit: Acquisitors',
       'description': '',
-      'roles':['E-Deposit: Acquisitor',]
-      },
+      'roles':[]
+  },
+    { 'group_id': 'Acquisition Administrators',
+      'title': 'E-Deposit: Acquisitor Administrators',
+      'description': 'Users that can assign work for acquisitors',
+      'roles':[]
+  },
     { 'group_id': 'Librarians',
       'title': 'E-Deposit: Librarians',
       'description': '',
-      'roles': ['E-Deposit: Librarian',]
-      },
-    { 'group_id': 'Producents',
-      'title': 'E-Deposit: Producents',
+      'roles': []
+  },
+    { 'group_id': 'Library Administrators',
+      'title': 'E-Deposit: Librarian Administrators',
       'description': '',
-      'roles': ['E-Deposit: Producent',]
-      },
+      'roles': []
+  },
+    { 'group_id': 'Producent Administrators',
+      'title': 'E-Deposit: Producent Administrators',
+      'description': '',
+      'roles': []
+  },
+    { 'group_id': 'Producent Contributors',
+      'title': 'E-Deposit: Producent Contributors',
+      'description': '',
+      'roles': []
+  },
+    { 'group_id': 'Producent Editors',
+      'title': 'E-Deposit: Producent Editors',
+      'description': '',
+      'roles': []
+  },
 )
 
 def setupGroups(portal):
@@ -28,8 +49,24 @@ def setupGroups(portal):
             gtool.addGroup(group['group_id'], roles=group['roles'])
             gtool.editGroup(group['group_id'], title = group['title'], description=group['description'])
 
+def createAgreementFile(portal):
+    #import sys,pdb; pdb.Pdb(stdout=sys.__stdout__).set_trace()
+    setup_tool = portal.portal_setup
+    profiles = setup_tool.listProfileInfo()
+    [ (pp['title'],pp['id']) for pp in  profiles]
+    [ pp['id'] for pp in  profiles]
+    linkName='smlouva-o-poskytovani-sluzeb'
+    if linkName not in portal.objectIds():
+        portal.invokeFactory('edeposit.user.agreementfile',  linkName,  title=u"Smlouva s Národní knihovnou")
+    pass
+
 def importVarious(context):
     """Miscellanous steps import handle
     """
+    if context.readDataFile('edeposit.policy.marker.txt') is None:
+        # Not your add-on
+        return
+
     portal = context.getSite()
     setupGroups(portal)
+    #createAgreementFile(portal)
--- edeposit/policy/testing.py
+++ edeposit/policy/testing.py
@@ -1,7 +1,9 @@
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import applyProfile
 from plone.app.testing import PLONE_FIXTURE
-from plone.app.testing import IntegrationTesting
+from plone.testing import z2
+from plone.app.testing import IntegrationTesting, FunctionalTesting
+from plone.app.robotframework.testing import AUTOLOGIN_LIBRARY_FIXTURE
 
 from zope.configuration import xmlconfig
 
@@ -15,8 +17,9 @@ class EDepositPolicy(PloneSandboxLayer):
         import edeposit.user
         import edeposit.content
         xmlconfig.file('configure.zcml', edeposit.policy, context=configurationContext)
+        xmlconfig.file('configure.zcml', edeposit.content, context=configurationContext) 
         xmlconfig.file('configure.zcml', edeposit.user, context=configurationContext)
-        xmlconfig.file('configure.zcml', edeposit.content, context=configurationContext)
+
 
         # # Install products that use an old-style initialize() function
         # z2.installProduct(app, 'Products.PythonField')
@@ -40,5 +43,11 @@ class EDepositPolicy(PloneSandboxLayer):
         applyProfile(portal, 'edeposit.user:default')
 
 EDEPOSIT_POLICY_FIXTURE = EDepositPolicy()
+EDEPOSIT_POLICY_ROBOT_TESTING = FunctionalTesting(
+    bases=(EDEPOSIT_POLICY_FIXTURE,
+           AUTOLOGIN_LIBRARY_FIXTURE,
+           z2.ZSERVER_FIXTURE),
+    name="E-Deposit Policy:Robot")
+
 EDEPOSIT_POLICY_INTEGRATION_TESTING = IntegrationTesting( bases=(EDEPOSIT_POLICY_FIXTURE,), 
                                                           name="EDeposit:Integration")
