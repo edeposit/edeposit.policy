44cd0162091479f02bbee9cb4bb97a9387024379
Jan Stavěl <stavel.jan@gmail.com>
2014-03-14 20:41:23 +0100

    added creating of new system message when transaction.

--- edeposit/policy/Extensions/epublication_wf_scripts.py
+++ edeposit/policy/Extensions/epublication_wf_scripts.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 from plone import api
+from plone.dexterity.utils import createContentInContainer, addContentToContainer, createContent
+import uuid
+
+def sendISBNCheckToAMQP(isbn):
+    return str(uuid.uuid1())
+
+def sendISBNCountToAMQP(isbn):
+    return str(uuid.uuid1())
+
 def submitISBNChecks(wfStateInfo):
     epublication = wfStateInfo.object
     epublication.objectItems()
@@ -9,7 +18,26 @@ def submitISBNChecks(wfStateInfo):
                    [ii.isbn for ii in originalFiles.results()])
     with api.env.adopt_user(username="system"):
         wft = api.portal.get_tool('portal_workflow')
+        systemMessages = epublication['system-messages']
         for isbn in isbns:
-            wft.doActionFor(epublication,"notifyISBNwasSentForValidation", comment="ISBN: " + isbn)
+            uuid = sendISBNCheckToAMQP(isbn)
+            wft.doActionFor(epublication,"notifyISBNwasSentForValidation",  
+                            comment="ISBN: " + isbn)
+            createContentInContainer(systemMessages,
+                                     'edeposit.content.isbncheckrequest',
+                                     title = "Kontrola ISBN: " + isbn,
+                                     uuid = uuid,
+                                     isbn = isbn
+                                 )
+            uuid = sendISBNCountToAMQP(isbn)
+            wft.doActionFor(epublication,"notifyISBNwasSentForCount", 
+                            comment="ISBN: " + isbn)
+            createContentInContainer(systemMessages,
+                                     'edeposit.content.isbncountrequest',
+                                     title = u"Zjištění duplicity ISBN: " + isbn,
+                                     uuid = sendISBNCountToAMQP(isbn),
+                                     isbn = isbn
+                                 )
+
         pass
     pass
