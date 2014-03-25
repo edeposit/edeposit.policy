b771be254a3950f11da7da2b4d417caee581a8e5
Jan Stavěl <stavel.jan@gmail.com>
2014-03-25 11:16:33 +0100

    added creating system message after submitting epublication.

--- edeposit/policy/Extensions/epublication_wf_scripts.py
+++ edeposit/policy/Extensions/epublication_wf_scripts.py
@@ -5,23 +5,22 @@ import uuid
 
 def submitISBNChecks(wfStateInfo):
     epublication = wfStateInfo.object
+    systemMessages = epublication['system-messages']
     originalFiles = epublication['original-files']
     isbns = filter(lambda item: item,
                    [epublication.isbn_souboru_publikaci] + \
                    [ii.isbn for ii in originalFiles.results()])
+
     with api.env.adopt_user(username="system"):
-        systemMessages = epublication['system-messages']
         for isbn in isbns:
-            createContentInContainer(systemMessages, 'edeposit.content.isbncheckrequest',
+            createContentInContainer(systemMessages, 'edeposit.content.isbnvalidationrequest',
                                      title = "Kontrola ISBN: " + isbn,
-                                     uuid = str(uuid.uuid4()),
                                      isbn = isbn
                                  )
             
             createContentInContainer(systemMessages,  'edeposit.content.isbncountrequest',
                                      title = u"Zjištění duplicity ISBN: " + isbn,
-                                     uuid = str(uuid.uuid4()),
                                      isbn = isbn
                                  )
+            pass
         pass
-    pass
