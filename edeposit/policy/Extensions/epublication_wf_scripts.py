9253eb4364be45931097ff136b8ec0afaf8789ae
Jan Stavěl <stavel.jan@gmail.com>
2014-03-27 15:18:09 +0100

    added new workflow scripts for isbn checking and antivirus

--- edeposit/policy/Extensions/epublication_wf_scripts.py
+++ edeposit/policy/Extensions/epublication_wf_scripts.py
@@ -2,8 +2,16 @@
 from plone import api
 from plone.dexterity.utils import createContentInContainer, addContentToContainer, createContent
 import uuid
+from logging import getLogger
+import itertools
+from functools import reduce
+
+
+logger = getLogger('edeposit.polici.wf_scripts')
 
 def submitISBNChecks(wfStateInfo):
+    logger.info("submitISBNChecks")
+    print "submit isbn checks"
     epublication = wfStateInfo.object
     systemMessages = epublication['system-messages']
     originalFiles = epublication['original-files']
@@ -22,5 +30,87 @@ def submitISBNChecks(wfStateInfo):
                                      title = u"Zjištění duplicity ISBN: " + isbn,
                                      isbn = isbn
                                  )
+            pass
+        pass
+
+def checkISBNsStatus(wfStateInfo):
+    logger.info("checkISBNsStatus")
+    print "check isbns status"
+    epublication = wfStateInfo.object
+    systemMessages = epublication['system-messages']
+    originalFiles = epublication['original-files']
+    wft = api.portal.get_tool('portal_workflow')
+    isbns = filter(lambda item: item,
+                   [epublication.isbn_souboru_publikaci] + \
+                   [ii.isbn for ii in originalFiles.results()])
+
+
+    def isbnISOK(result,item):
+        k,v = item
+
+        def findProperMessage(result, item):
+            msg = item[1]
+            if hasattr(msg,'is_valid'):
+                result['is_valid'].add(msg.is_valid)
+            if hasattr(msg,'num_of_records'): 
+                result['num_of_records'].add(msg.num_of_records)
+            return result
+
+        properMessages = reduce( findProperMessage, v, {'is_valid':set(), 'num_of_records': set()} )
+        # alespon jeden is_valid
+        # vsechny is_valid musi byt OK
+
+        # alespon jeden num_of_records
+        # vsechny musi byt 0
+        result[k] = properMessages
+        return result
+
+    with api.env.adopt_user(username="system"):
+        # for each isbn must exists request and response in system
+        # messages
+        itemsByISBN = reduce( isbnISOK,
+                              itertools.groupby(systemMessages.items(), key=lambda item: item[1].isbn),
+                              dict() )
+
+        # isbns = ['978-0-306-40615-7',]
+        # itemsByISBN = {'978-0-306-40615-7': {'num_of_records': set([0]), 'is_valid': set([True])}}
+        # statuses = itemsByISBN['978-0-306-40615-7']
+        
+        def isbnISOK(isbn):
+            statuses = itemsByISBN.get(isbn,{'num_of_records':set(),'is_valid':set()})
+            return statuses['is_valid'] == set([True]) and statuses['num_of_records'] == set([0])
+
+        isbnStatuses = [ (isbn,isbnISOK(isbn)) for isbn in isbns]
+        if set([ii[1] for ii in isbnStatuses]) == set([True]):
+            # vsechny ISBN jsou zkontrolovane
+            print "all isbns are valid"
+            wft.doActionFor(epublication, 'allISBNsAreValid')
+            pass
+
+        # existuje nejake s chybou?
+        #withError = filter(lambda item: item[0]
+        
+        pass
+    pass
+
+
+def submitAntivirusChecks(wfStateInfo):
+    logger.info("submitAntivirusChecks")
+    print "submit antivirus checks"
+    epublication = wfStateInfo.object
+    systemMessages = epublication['system-messages']
+    originalFiles = epublication['original-files']
+
+    with api.env.adopt_user(username="system"):
+        for originalFile in originalFiles:
+            # createContentInContainer(systemMessages, 'edeposit.content.isbnvalidationrequest',
+            #                          title = "Kontrola ISBN: " + isbn,
+            #                          isbn = isbn
+            #                      )
+            
+            # createContentInContainer(systemMessages,  'edeposit.content.isbncountrequest',
+            #                          title = u"Zjištění duplicity ISBN: " + isbn,
+            #                          isbn = isbn
+            #                      )
             pass
         pass
