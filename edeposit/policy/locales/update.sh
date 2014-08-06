ffbb38fdfb2e0cf79776eb6420084a5dec53c57c
Jan Stavěl <stavel.jan@gmail.com>
2014-08-06 13:24:29 +0200

    added czech translation for "Required input is missing."  jstavel/edeposit#30

--- edeposit/policy/locales/update.sh
+++ edeposit/policy/locales/update.sh
@@ -5,3 +5,4 @@ domain=edeposit.policy
 ../../../../../bin/i18ndude rebuild-pot --pot plone.pot --create plone ../profiles
 ../../../../../bin/i18ndude sync --pot plone.pot cs/LC_MESSAGES/plone.po
 msgfmt -o cs/LC_MESSAGES/plone.mo cs/LC_MESSAGES/plone.po
+msgfmt -o cs/LC_MESSAGES/plone.mo cs/LC_MESSAGES/zope.po
