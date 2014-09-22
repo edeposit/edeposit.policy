882ee3d783c174e54ab4ca9033e49b71c21a89ef
Jan Stavěl <stavel.jan@gmail.com>
2014-09-22 13:32:48 +0200

    na testovacim serveru je producents folder ve stavu published.  Tento stav jsem zrusil pri vyvoji. Takze tedka je problem v datech. Reseni: doplnil jsem stav published. jstavel/edeposit#59

--- edeposit/policy/Extensions/producentfolder_wf_scripts.py
+++ edeposit/policy/Extensions/producentfolder_wf_scripts.py
@@ -10,6 +10,7 @@ from z3c.relationfield import RelationValue
 from Acquisition import aq_inner, aq_parent
 from zope.component import queryUtility, getUtility, getAdapter
 import base64
+from edeposit.policy import MessageFactory as _
 
 from edeposit.content.amqp import IAMQPSender, IAMQPHandler
 
@@ -22,10 +23,10 @@ logger = getLogger('edeposit.producentfolder_wf_scripts')
 # (occur-1 "class " nil (list (current-buffer)) "*Occur: originalfile_wf_scripts.py/class*")
 
 def sendEmailFactory(view_name, recipients, subject):
-    def handler(wfStateInto):
+    def handler(wfStateInfo):
         context = wfStateInfo.object
         view = api.content.get_view(name=view_name, context = context, request = context.REQUEST)
-        api.portal.send_email(recipient=recipient, subject=subject, body=view())
+        api.portal.send_email(recipient=",".join(recipients), subject=subject, body=view())
     return handler
 
 sendEmailToISBNAgency = sendEmailFactory("worklist-for-isbn-agency",
@@ -55,45 +56,12 @@ def recreateCollections(wfStateInfo):
                   'v': '../'}
                  ]
 
-    collections = [ dict( name  = "producents-in-declaring",
-                          title = _(u"Producents in declaring"),
-                          query = [ {'i': 'portal_type',
-                                     'o': 'plone.app.querystring.operation.selection.is',
-                                     'v': ['edeposit.user.producent',]},
-                                    {'i': 'review_state',
-                                     'o': 'plone.app.querystring.operation.selection.is',
-                                     'v': ['registration',]},
-                                    {'i': 'path', 
-                                     'o': 'plone.app.querystring.operation.string.relativePath', 
-                                     'v': '../'}
-                                    ],
-                          roles = ['E-Deposit: Producent Administrator','E-Deposit: Acquisitor'],
-                          ),
-                    dict( name = "approved-producents",
-                          title=_(u"Approved producents"),
-                          query=[{'i': 'portal_type', 
-                                  'o': 'plone.app.querystring.operation.selection.is', 
-                                  'v': ['edeposit.user.producent',] },
-                                 {'i': 'review_state',
-                                  'o': 'plone.app.querystring.operation.selection.is',
-                                  'v': ['approved',]},
-                                 {'i': 'path', 
-                                  'o': 'plone.app.querystring.operation.string.relativePath', 
-                                  'v': '../'}
-                                 ],
-                          roles = ['E-Deposit: Producent Administrator','E-Deposit: Acquisitor'],
-                          ),
-
-                    dict( name = "originalfiles-for-isbn-agency",
-                          title=_(u"Dokumenty čekající na přidělení ISBN"),
-                          query= queryForStates('declaration'),
-                          roles = ['E-Deposit: Producent Administrator','E-Deposit: Acquisitor','E-Deposit: ISBN Agency'],
-                          ),
-                    dict( name = "ePublications-waiting-for-isbn-generation",
-                          title = _(u"ePublications waiting for preparing of acquisition"),
-                          query= queryForStates('ISBNGeneration'),
-                          roles = ['E-Deposit: Producent Administrator','E-Deposit: System', 'E-Deposit: Acquisitor', 'E-Deposit: ISBN Agency'],
-                          ),
+    collections = [ 
+        dict( name = "originalfiles-for-isbn-agency",
+              title= u"Dokumenty čekající na přidělení ISBN",
+              query= queryForStates('ISBNGeneration'),
+              roles = ['E-Deposit: Producent Administrator','E-Deposit: Acquisitor','E-Deposit: ISBN Agency'],
+          ),
     ]
 
     for collection in collections:
